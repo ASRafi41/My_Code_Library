d4ed306c8dac53da7d27f073dd93a758ed1135a6
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2023-05-20 15:12:18 +0600

    Update Ordered_Set (Policy based data structures).cpp

--- ETC/Ordered_Set (Policy based data structures).cpp
+++ ETC/Ordered_Set (Policy based data structures).cpp
@@ -5,33 +5,28 @@ using namespace std;
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
+
 template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T, typename R> using ordered_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // *s.find_by_order(k): K-th element in a set (counting from zero).
 // s.order_of_key(k): Number of items strictly smaller than k. (same as lower_bound of k)
+// less_equal<T> => for ordered_multiset or, ordered_multimap
 
 int main()
 {
-    ll q, v;
     ordered_set<ll> s;
+    s.insert(1);
+    s.insert(2);
+    cout << *s.find_by_order(0) << endl; // k th element
+    cout << s.order_of_key(2) << endl;   // number of elements less than k
 
-    cin >> q;
-    while(q--)
-    {
-        char ch;
-        cin >> ch >> v;
-
-        if (ch == 'I') s.insert(v);
-        else if (ch == 'D') s.erase(v);
-        else if (ch == 'C') //return the number of elements of S smaller than x
-        {
-            cout << s.order_of_key(v) << "\n";
-        }
-        else if (ch == 'K')
-        {
-            if (v > s.size()) cout << "invalid\n";
-            else cout << *s.find_by_order(v - 1) << "\n";
-        }
-    }
+    ordered_map<int, ll> mp;
+    mp.insert({1, 10});
+    mp.insert({2, 20});
+    cout << mp.find_by_order(0)->second << endl; // k th element
+    cout << mp.order_of_key(2) << endl;          // number of first elements less than k
+    
     return 0;
 }
