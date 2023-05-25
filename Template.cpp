7765ce05ef67f673a77b93d031a9b4ebe087dce7
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2023-05-25 16:05:05 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,4 +1,6 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 typedef long long       ll;
 #define endl            '\n'
 #define F               first
@@ -15,22 +17,33 @@ typedef long long       ll;
 #define ClearBit(x,k)   (x &= ~(1LL << k))
 #define MSB(mask)       63-__builtin_clzll(mask) 
 #define LSB(mask)       __builtin_ctzll(mask)  
-#define error(x)        cerr << #x << " = " << (x) <<endl
-#define Error(a,b)      cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n";
-#define coutall(v)      for(auto &it: v)cout<<it<<' ';cout<<endl;
+#define error1(x)       cerr << #x << " = " << (x) <<endl
+#define error2(a,b)     cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n";
+#define coutall(v)      for(auto &it: v) cout<<it<<' '; cout<<endl;
 #define _ASRafi__       ios::sync_with_stdio(false);cin.tie(0),cin.tie(0);
 using namespace std;
+using namespace __gnu_pbds;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); /// mt19937_64 (long long)
+auto my_rand(long long l,long long r)   // random value generator [l, r]
+{
+    return uniform_int_distribution<long long>(l,r)(rng);
+}
+
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T, typename R> using ordered_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // const int fx[] = {+0, +0, +1, -1, -1, +1, -1, +1}; // king's move(0 to 3 index=> Side Moves)
 // const int fy[] = {-1, +1, +0, +0, +1, +1, -1, -1}; // king's move(4 to 7 index=> Diagonal Moves)
 // const int kx[] = {-2, -2, -1, -1, +1, +1, +2, +2}; // knight's move
 // const int ky[] = {-1, +1, -2, +2, -2, +2, -1, +1}; // knight's move
+
 // const long double pi = 2 * acos(0.0);
 // const ll N = 1e7 + 10, mod = 1e9 + 7;
 
 void solve()
 {
-    ll n, m, k1, c = 0, ans = 0;
+    ll n, m, k, c = 0, ans = 0;
     string s1, s2;
     cin >> n;
     vector<ll> v(n);
