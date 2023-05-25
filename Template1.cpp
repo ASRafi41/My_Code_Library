a61b7b746d55620b729955adb0a2ffef5dbb8a3b
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2023-05-25 16:17:48 +0600

    Create Template1.cpp

---
+++ Template1.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+typedef long long   ll;
+#define endl        '\n'
+#define F           first
+#define S           second
+#define pb          push_back
+#define ppb         pop_back
+#define yes         cout<<"YES\n"
+#define no          cout<<"NO\n"
+#define all(x)      x.begin(),x.end()
+#define allr(x)     x.rbegin(),x.rend()
+#define error1(x)   cerr << #x << " = " << (x) <<endl
+#define error2(a,b) cerr<<"( "<<#a<<" , "<<#b<<" ) = ( "<<(a)<<" , "<<(b)<<" )\n";
+#define coutall(v)  for(auto &it: v) cout<<it<<' '; cout<<endl;
+#define _ASRafi__   ios::sync_with_stdio(false);cin.tie(0),cin.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n, m, k, c = 0, ans = 0;
+    string s1, s2;
+    cin >> n;
+    vector<ll> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        ll x; cin >> x;
+        v[i] = x;
+        
+    }
+    
+    return;
+}
+int32_t main()
+{
+    _ASRafi__;
+    int tc = 1;
+    cin >> tc;
+    while (tc--) solve();
+    return 0;
+}
