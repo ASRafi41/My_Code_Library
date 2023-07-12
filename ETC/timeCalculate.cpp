96dcb1d3665f8f93870e9e82e2e804361540c201
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2023-07-13 04:41:35 +0600

    Add files via upload

---
+++ ETC/timeCalculate.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    auto st = clock(); // clock() -> return current time
+    // Code
+    // .
+    // .
+    // .
+    cerr << "Time = " << 1.0 * (clock() - st) / CLOCKS_PER_SEC << "\n";
+    
+    /* if array size if N then the memory
+    occupied by the array will be = (N * DataType byte) / 10^6;
+    for example : if N = 10^8 array is int arr[N] then, memory=(10^8 * 4) / 10^6 = 400Mb */
+}
