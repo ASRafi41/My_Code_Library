44c7900fd0930270e120019fe41acb9296e1d28f
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2023-08-31 13:58:26 +0600

    Update nCr .cpp

--- Number Theory/nCr .cpp
+++ Number Theory/nCr .cpp
@@ -3,27 +3,27 @@
 #define ll long long
 using namespace std;
 
-// Function to find the nCr     =>O(r*log(n))
+// Function to find the nCr     => O(r * log(n))
 ll nCr(ll n, ll r)
 {
-    ll p= 1, k=1, m;
-    if(n-r<r)r=n-r;
-    if(r!=0)
+    ll p = 1, k = 1, m;
+    if (n - r < r) r = n - r;
+    if (r != 0)
     {
-        while(r)
+        while (r)
         {
-            p*=n, k*=r;
-            m=__gcd(p, k);
-            p/=m, k/=m;
+            p *= n, k *= r;
+            m = __gcd(p, k);
+            p /= m, k /= m;
             n--, r--;
         }
     }
-    else p=1;
+    else p = 1;
     return p;
 }
 int main()
 {
-    long long n,r;
-    cin>>n>>r;
+    long long n, r;
+    cin >> n >> r;
     nCr(n, r);
 }
