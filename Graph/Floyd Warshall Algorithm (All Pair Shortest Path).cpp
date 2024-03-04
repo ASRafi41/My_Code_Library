593b74ad718ba3ca3b5bbe92a93099e99e0b4294
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2024-03-04 22:03:53 +0600

    Update Floyd Warshall Algorithm (All Pair Shortest Path).cpp

--- Graph/Floyd Warshall Algorithm (All Pair Shortest Path).cpp
+++ Graph/Floyd Warshall Algorithm (All Pair Shortest Path).cpp
@@ -1,49 +1,59 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int N=510, INF=1e9+10;
-int dp[N][N];
-int n,m; 
+// Floyd-Warshall Algorithm - Used to find the shortest paths between all pairs of nodes in a graph.
+// Time Complexity = V * V * V
 
-void floyd_warshall()
+const int N = 510, INF = 1e9 + 10;
+int dp[N][N];
+int n, m;
+
+void floydWarshall()
 {
-    for (int k = 1; k <= n; ++k) 
+    for (int k = 1; k <= n; ++k)
     {
-        for (int i = 1; i <= n; ++i) 
+        for (int i = 1; i <= n; ++i)
         {
-            for (int j = 1; j <= n; ++j) 
+            for (int j = 1; j <= n; ++j)
             {
                 if (dp[i][k] < INF && dp[k][j] < INF)
-                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]); 
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
             }
         }
     }
 }
+
 int main()
 {
-    cin>>n>>m;
-    for(int i=1;i<=n; ++i)
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; ++i) // reset
     {
-        for(int j=1;j<=n; ++j)
+        for (int j = 1; j <= n; ++j)
         {
-            if(i==j) dp[i][j]=0;
-            else dp[i][j]=INF;
+            if (i == j) dp[i][j] = 0;
+            else dp[i][j] = INF;
         }
     }
-    for(int i=0;i<m;i++)
+
+    for (int i = 0; i < m; i++)
     {
-        int x,y,wt; cin>>x>>y>>wt;
-        dp[x][y]=wt;
+        int x, y, wt;
+        cin >> x >> y >> wt;
+        dp[x][y] = wt;
+        // dp[y][x] = wt;
     }
-    floyd_warshall();
-    for(int i=1; i<=n; ++i)
+
+    floydWarshall();
+    
+    for (int i = 1; i <= n; ++i)
     {
-        for(int j=1; j<=n; ++j)
+        for (int j = 1; j <= n; ++j)
         {
-            if(dp[i][j]==INF) cout<<"i ";
-            else cout<<dp[i][j]<<" ";
+            if (dp[i][j] == INF) cout << "i ";
+            else cout << dp[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
