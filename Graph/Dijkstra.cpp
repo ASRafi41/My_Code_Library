a6c14e5a480a55a9aea42f05238c7dadc159b5c2
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2024-04-07 17:23:05 +0600

    Update Dijkstra.cpp

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -5,14 +5,14 @@ using namespace std;
 // Dijkstra's Algorithm - Used to find the shortest path between nodes in a graph with non-negative edge weights.
 // Time Complexity = (V + E) * Log(V)
 
-const int INF = INT_MAX;
-vector<vector<pair<int, int>>> g;
+const ll INF = LLONG_MAX;
+vector<vector<pair<ll, ll>>> g;
 
-void dijkstra(int s, vector<ll> &d, vector<ll> &p)
+void dijkstra(ll s, vector<ll> &d, vector<ll> &p)
 {
-    int n = g.size();
-    d.assign(n, INF);
-    p.assign(n, -1);
+    ll n = g.size();
+    d.assign(n + 1, INF);
+    p.assign(n + 1, -1);
 
     d[s] = 0;
     priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> q;
@@ -35,11 +35,10 @@ void dijkstra(int s, vector<ll> &d, vector<ll> &p)
     }
 }
 
-vector<ll> restore_path(int s, int dest, vector<ll> const &p)
+vector<ll> restore_path(ll s, ll dest, vector<ll> const &p)
 {
     vector<ll> path;
-
-    for (auto v = dest; v != s && v != -1; v = p[v]) path.push_back(v);
+    for (auto v = dest; v != s && p[v] != -1; v = p[v]) path.push_back(v);
     path.push_back(s);
 
     reverse(path.begin(), path.end());
@@ -48,6 +47,7 @@ vector<ll> restore_path(int s, int dest, vector<ll> const &p)
 
 int main()
 {
+    ios::sync_with_stdio(false); cin.tie(0);
     int n, m, source = 1;
     cin >> n >> m;
     g.assign(n + 1, {}); // <===
@@ -56,6 +56,7 @@ int main()
         int u, v, w;
         cin >> u >> v >> w;
         g[u].push_back({v, w});
+        g[v].push_back({u, w});
     }
     // -- Print List --
     // for (int i = 1; i <= n; i++)
@@ -71,10 +72,30 @@ int main()
     cout << "-> Distance <-" << endl;
     for (int i = 1; i <= n; i++) cout << source << " to " << i << " => " << dist[i] << endl;
 
-    cout << "-> Source to Destination Path <- " << endl;
-    int dest = 4;
+    cout << "\n-> Source to Destination Path <- " << endl;
+    int dest = n; // <==
     auto path = restore_path(source, dest, Paths);
+    if(path.back() != dest)
+    {
+        cout << -1 << endl;
+        return 0;
+    }
     for(int i = 0; i < path.size(); i++) cout << path[i] << (i + 1 == path.size() ? "\n" : " -> ");
 
     return 0;
 }
+
+/*Test Case:
+6 9
+1 2 1
+1 3 5
+2 3 2
+2 4 2
+2 5 1
+3 5 2
+4 5 3
+4 6 1
+5 6 2
+
+Output:  Distance => 0, 1, 3, 3, 2, 4,
+*/
