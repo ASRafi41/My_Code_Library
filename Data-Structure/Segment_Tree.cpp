7392822e7b501ff46d5f68af243f6beec88455ff
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2024-08-21 16:08:42 +0600

    Update Segment_Tree.cpp

--- Data-Structure/Segment_Tree.cpp
+++ Data-Structure/Segment_Tree.cpp
@@ -1,95 +1,89 @@
 #include<bits/stdc++.h>
 typedef long long ll;
 #define endl '\n'
-#define _ASRafi_ ios::sync_with_stdio(false);cin.tie(0),cin.tie(0);
 using namespace std;
 
 const int N = 3e5 + 9;
-ll a[N];
-ll t[4 * N];
 
-void build(int node, int st, int en)    //=> O(N)
-{
-    if (st == en) {
-        t[node] = a[st];
+struct Segment_Tree {
+    ll t[4 * N];
+
+    auto merge(auto &l, auto &r) { // <= Change this function
+        return l + r;
+    }
+    void build(int node, int st, int en, vector<ll> &arr) { //=> O(N)
+        if (st == en) {
+            t[node] = arr[st];
+            return;
+        }
+        int mid = (st + en) >> 1;
+        build(node << 1, st, mid, arr); // divide left side
+        build(node << 1 | 1, mid + 1, en, arr); // divide right side
+        // Merging left and right portion
+        auto &Cur = t[node];
+        auto &Left = t[node << 1];
+        auto &Right = t[node << 1 | 1];
+        Cur = merge(Left, Right);
         return;
     }
-    int mid = (st + en) >> 1;
-    build(2 * node, st, mid); // divide left side
-    build(2 * node + 1, mid + 1, en); // divide right side
-    // Merging left and right portion
-    auto &Cur = t[node];
-    auto &Left = t[2 * node];
-    auto &Right = t[2 * node + 1];
-    Cur = Left + Right;
-}
-ll query(int node, int st, int en, int l, int r)   //=> O(log n)
-{
-    if (st > r || en < l) // No overlapping and out of range
-    {
-        return 0; // <== careful 
-    }
-    if (l <= st && en <= r) // Complete overlapped (l-r in range)
-    {
-        return t[node];
-    }
-    // Partial overlapping
-    int mid = (st + en) >> 1;
-    auto Left = query(2 * node, st, mid, l, r);
-    auto Right = query(2 * node + 1, mid + 1, en, l, r);
-    return Left + Right;
-}
-void update(int node, int st, int en, int idx, ll val) //=> O(log n)
-{
-    if (st == en) {
-        a[st] = val;
-        t[node] = val;
+    void update(int node, int st, int en, int idx, ll val) { //=> O(log n)
+        if (st == en) {
+            t[node] = val;
+            return;
+        }
+        int mid = (st + en) >> 1;
+        if (idx <= mid) update(node << 1, st, mid, idx, val);
+        else update(node << 1 | 1, mid + 1, en, idx, val);
+        // Merging left and right portion
+        auto &Cur = t[node];
+        auto &Left = t[node << 1];
+        auto &Right = t[node << 1 | 1];
+        Cur = merge(Left, Right);
         return;
     }
-    int mid = (st + en) >> 1;
-    if (idx <= mid) update(2 * node, st, mid, idx, val);
-    else update(2 * node + 1, mid + 1, en, idx, val);
-    // Merging left and right portion
-    auto &Cur = t[node];
-    auto &Left = t[2 * node];
-    auto &Right = t[2 * node + 1];
-    Cur = Left + Right;
-}
+    ll query(int node, int st, int en, int l, int r) { //=> O(log n)
+        if (st > r || en < l) { // No overlapping and out of range
+            return 0; // <== careful 
+        }
+        if (l <= st && en <= r) { // Complete overlapped (l-r in range)
+            return t[node];
+        }
+        // Partial overlapping
+        int mid = (st + en) >> 1;
+        auto Left = query(node << 1, st, mid, l, r);
+        auto Right = query(node << 1 | 1, mid + 1, en, l, r);
+        return merge(Left, Right);
+    }
+} sgt;
 
-void solve()
-{
+void solve() {
     ll n, q;
     cin >> n >> q;
-
-    for (int i = 1; i <= n; i++)
-    {
+    vector<ll> v(n + 1);
+    for (int i = 1; i <= n; i++) {
         ll x; cin >> x;
-        a[i] = x;
+        v[i] = x;
     }
-    build(1, 1, n); // Creating Segment tree;
-    while (q--)
-    {
-        int type; cin >> type;
-        if (type == 1)
-        {
+    sgt.build(1, 1, n, v); // Creating Segment tree;
+    while (q--) {
+        short type; cin >> type;
+        if (type == 1) {
             int i, val; cin >> i >> val;
-            update(1, 1, n, i, a[i] + val);
+            sgt.update(1, 1, n, i, v[i] + val);
         }
-        else
-        {
+        else {
             int l, r; cin >> l >> r;
-            cout << query(1, 1, n, l, r) << endl;
+            cout << sgt.query(1, 1, n, l, r) << endl;
         }
     }
     return;
 }
-int main()
-{
-    _ASRafi_;
+
+signed main() {
+    ios::sync_with_stdio(false);cin.tie(0), cin.tie(0);
     int tc = 1;
-    cin >> tc;
-    for (int i = 1; i <= tc; ++i)
-    {
+    // cin >> tc;
+    for (int i = 1; i <= tc; ++i) {
         // cout << "Case " << i << ":\n";
         solve();
     }
