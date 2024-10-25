ba0a1d5a7591a2936d98ac15dcb4c89d4a04b714
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2024-10-25 22:55:11 +0600

    Update Trie.cpp

--- Data-Structure/Trie.cpp
+++ Data-Structure/Trie.cpp
@@ -1,76 +1,98 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct node
-{
-    node *next[26];
-    bool completedWord;
-    node()
-    {
-        completedWord = false;
-        for (int i = 0; i < 26; i++)
-            next[i] = NULL;
-    }
-} *root;
 
-void trieInsert(string s)
-{
-    node *cur = root;
-    for (int i = 0; i < s.size(); i++)
-    {
-        int x = s[i] - 'a';
-        if (cur->next[x] == NULL)
-        {
-            cur->next[x] = new node();
+struct Trie {
+    static const int rangeSize = 26; // for lower_case letter ('a' <= 'z')
+
+    struct node {
+        node *next[rangeSize]; 
+        bool completedWord;
+        int cnt;
+        node() {
+            completedWord = false;
+            cnt = 0;
+            for (int i = 0; i < rangeSize; i++)
+                next[i] = nullptr;
         }
-        cur = cur->next[x];
-    }
-    cur->completedWord = true;
-}
-bool trieSearch(string s)
-{
-    node *cur = root;
-    for (int i = 0; i < s.size(); i++)
-    {
-        int x = s[i] - 'a';
-        if (cur->next[x] == NULL)
-        {
-            return false;
-        }
-        cur = cur->next[x];
-    }
-    return cur->completedWord;
-}
-void reset(node* cur)   //Delete the tree after every test case for memory efficiency
-{
-    for(int i=0; i<26; i++)
-        if(cur->next[i]) 
-            reset(cur->next[i]);
-    delete(cur);
-}
-int main()
-{
-    int tc=1;
-    cin>>tc;
-    while (tc--)
-    {
+    } *root;
+
+    Trie() {
         root = new node();
-        int n, q;
-        cin >> n >> q;
-        string s;
-        for (int i = 0; i < n; i++)
-        {
-            cin >> s;
-            trieInsert(s);
+    }
+
+    void trieInsert(const string &s) {
+        node *cur = root;
+        for (char ch : s) {
+            int x = ch - 'a'; // for lowercase letter
+            if (cur->next[x] == nullptr) {
+                cur->next[x] = new node();
+            }
+            cur = cur->next[x];
+            cur->cnt += 1;
         }
-        while (q--)
-        {
-            cin >> s;
-            if (trieSearch(s))
-                cout << "Found\n";
-            else
-                cout << "Not Found\n";
+        cur->completedWord = true;
+    }
+
+    bool trieSearch(const string &s) {
+        node *cur = root;
+        for (char ch : s) {
+            int x = ch - 'a'; // for lowercase letter
+            if (cur->next[x] == nullptr) {
+                return false;
+            }
+            cur = cur->next[x];
         }
+        return cur->completedWord;
+    }
+
+    int prefixCount(const string &s) {
+        node *cur = root;
+        for (char ch : s) {
+            int x = ch - 'a'; // for lowercase letter
+            if (cur->next[x] == nullptr) {
+                return 0;
+            }
+            cur = cur->next[x];
+        }
+        return cur->cnt;
+    }
+
+    void reset(node* cur) {
+        for(int i = 0; i < rangeSize; i++)
+            if(cur->next[i])
+                reset(cur->next[i]);
+        delete cur;
+    }
+
+    void clear() {
+        reset(root); // Delete all nodes
+        root = new node(); // Re-initialize root node for reuse
+    }
+
+    ~Trie() { // Destructor 
         reset(root);
+    }
+} trie;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int tc = 1;
+    cin >> tc;
+    while (tc--) {
+        trie.clear(); // <===
+        int n, q;
+        cin >> n;
+        string s;
+        for (int i = 0; i < n; i++) {
+            cin >> s;
+            trie.trieInsert(s);
+        }
+        cin >> q;
+        while (q--) {
+            cin >> s;
+            if (trie.trieSearch(s)) cout << "Found\n";
+            else cout << "Not Found\n";
+        }
     }
     return 0;
 }
