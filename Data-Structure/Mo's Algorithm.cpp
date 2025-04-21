1b72c229e7c1211de3dd7cc9dcc6d66547ec13d8
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2025-04-21 06:41:05 +0600

    Update Mo's Algorithm.cpp

--- Data-Structure/Mo's Algorithm.cpp
+++ Data-Structure/Mo's Algorithm.cpp
@@ -1,30 +1,39 @@
-#include <bits/stdc++.h>    //=> O((N+Q) * sqrt(N))
-#define endl "\n"
-#define ll long long
+// Time Complexity: O((N+Q) * sqrt(N))
+#include <bits/stdc++.h>    
+#define endl '\n'
+using ll = long long;
 using namespace std;
 
-const int N = 1e6 + 7;
-int rootN;
+const int N = 1e6 + 3;
+int a[N], rootN;
+
+int block_get(int ind) {
+    return ind / rootN;
+}
+int block_get_start(int block) {
+    return (rootN * block);
+}
+int block_get_end(int block) {
+    return min(N - 1, rootN * (block + 1) - 1);
+}
 
 struct Q {
     int l, r, idx;
 };
 Q q[N];
 
-bool comp(Q q1, Q q2) {
-    if (q1.l / rootN == q2.l / rootN) return q1.r > q2.r;
-    return q1.l / rootN < q2.l / rootN;
-}
-
-// bool comp(Q p, Q q) { // faster 
-//     if (p.l / rootN != q.l / rootN) return (p.l == q.l ? p.r <= q.r : p.l < q.l); // return p < q;
-//     return (p.l / rootN & 1) ? (p.r < q.r) : (p.r > q.r);
+// bool comp(Q &q1, Q &q2) {
+//     if (q1.l / rootN == q2.l / rootN) return q1.r > q2.r;
+//     return q1.l / rootN < q2.l / rootN;
 // }
+bool comp(Q &p, Q &q) { // faster 
+    if (p.l / rootN != q.l / rootN) return (p.l == q.l ? p.r <= q.r : p.l < q.l); // return p < q;
+    return (p.l / rootN & 1) ? (p.r < q.r) : (p.r > q.r);
+}
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
     int n; cin >> n;
-    int a[n];
     for (int i = 0; i < n; ++i) cin >> a[i];
     int query; cin >> query;
     rootN = sqrtl(n) + 1;
@@ -42,24 +51,35 @@ int main() {
     for (int i = 0; i < query; i++) {
         l = q[i].l, r = q[i].r;
         --l, --r;
+
+        auto include = [&](int idx) -> void {
+            curr_ans += a[idx];
+        };
         while (curr_r < r) {
             ++curr_r;
-            curr_ans += a[curr_r];
+            include(curr_r);
         }
         while (curr_l > l) {
             --curr_l;
-            curr_ans += a[curr_l];
+            include(curr_l);
         }
         
+        auto exclude = [&](int idx) -> void {
+            curr_ans -= a[idx];
+        };
         while (curr_l < l) {
-            curr_ans -= a[curr_l];
+            exclude(curr_l);
             ++curr_l;
         }
         while (curr_r > r) {
-            curr_ans -= a[curr_r];
+            exclude(curr_r);
             --curr_r;
         }
-        ans[q[i].idx] = curr_ans;
+
+        auto getAns = [&]() -> ll {
+            return curr_ans;
+        };
+        ans[q[i].idx] = getAns();
     }
     
     for (int i = 0; i < query; i++) {
@@ -67,3 +87,4 @@ int main() {
     }
     return 0;
 }
+// Mo's + Square root decomposition => https://codeforces.com/contest/1000/submission/316444520
