cba7cd474c9bf2441a560b7d5c191443d88fc0be
Abu Sufian Rafi <130089874+ASRafi41@users.noreply.github.com>
2025-07-26 10:56:21 +0600

    Update Square Root Decomposition.cpp

--- Data-Structure/Square Root Decomposition.cpp
+++ Data-Structure/Square Root Decomposition.cpp
@@ -1,47 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 // min value problem
-int main()
-{
+const int N = 1e6 + 3;
+int a[N], rootN;
+
+int block_get(int ind) {
+    return ind / rootN;
+}
+int block_get_start(int block) {
+    return (rootN * block);
+}
+int block_get_end(int block) {
+    return min(N - 1, rootN * (block + 1) - 1);
+}
+
+int main() {
     int n;
     cin >> n;
     int a[n];
-    for (int i = 0; i < n; ++i)
-        cin >> a[i];
+    for (int i = 0; i < n; ++i) cin >> a[i];
 
     // preprocessing
     int rootN = sqrtl(n) + 1;
     int block[rootN];
     for (int i = 0; i < rootN; ++i) block[i] = INT_MAX;
-    for (int i = 0; i < n; ++i)
-    {
-        // block[i/rootN]+= a[i];   //sum
+    for (int i = 0; i < n; ++i) {
+        // block[i/rootN] += a[i];   // for sum
         block[i / rootN] = min(block[i / rootN], a[i]);
     }
 
     // answering the queries
     int q;
     cin >> q;
-    while (q--) //=> O(q*sqrt(n))
-    {
+    while (q--) { //=> O(q*sqrt(n))
         int l, r, ans = INT_MAX, sum = 0;
         cin >> l >> r;
-        for (int i = l; i <= r;) //=> O(sqrt(n))
-        {
-            if (i % rootN == 0 && i + rootN - 1 <= r)
-            {
-                // sum+=block(i/rootN);
+        for (int i = l; i <= r;) { //=> O(sqrt(n))
+            if (i % rootN == 0 && i + rootN - 1 <= r) {
+                // sum += block(i / rootN);
                 ans = min(ans, block[i / rootN]);
                 i += rootN;
             }
-            else
-            {
-                // sum+=a[i];
+            else {
+                // sum += a[i];
                 ans = min(ans, a[i]);
                 ++i;
             }
         }
-        cout << ans << endl;
+        cout << ans << "\n";
     }
     return 0;
 }
